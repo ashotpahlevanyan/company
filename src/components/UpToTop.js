2ccbd0887c1501dd8f29e6717c4b09613299b59c
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-06-12 02:15:16 +0400

    changed colors of uptotop

--- src/components/UpToTop.js
+++ src/components/UpToTop.js
@@ -8,41 +8,38 @@ class UpTopTop extends Component {
 		this.state = {
 			scrollTop: 0
 		};
+
 		this.handleClick = this.handleClick.bind(this);
-		// this.myFunction = this.myFunction.bind(this);
-		// this.applyRef = this.applyRef.bind(this);
+		//this.handleScroll = this.handleScroll.bind(this);
 	}
 
-	// myFunction() {
-	// 	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-	// 		console.log(document.body.scrollTop);
-	// 		this.setState({class: 'center noPrint upToTop'});
-	// 	} else {
-	// 		console.log(document.body.scrollTop);
-	// 		this.setState({class: 'center noPrint upToTop hide'});
-	// 	}
-	// }
 	// componentDidMount() {
-	// 	window.addEventListener('scroll', this.myFunction);
-	//
+	// 	window.addEventListener('scroll', this.handleScroll);
 	// }
 	// componentWillUnmount() {
-	// 	window.removeEventListener('scroll', this.myFunction);
+	// 	window.addEventListener('scroll', this.handleScroll);
+	// }
+	// handleScroll() {
+	// 		let winHeight = window.innerHeight;
+	//
+	// 		// Annoying to compute doc height due to browser inconsistency
+	// 		let body = document.body;
+	// 		let html = document.documentElement;
+	// 		let docHeight = Math.max( body.scrollHeight, body.offsetHeight,
+	// 			html.clientHeight, html.scrollHeight, html.offsetHeight );
+	//
+	// 		let value = document.body.scrollTop;
+	// 		this.setState({scrollTop: docHeight});
 	// }
 	handleClick = function() {
 		window.scrollTo({
 			top: 0,
 			behavior: "smooth"
 		});
-		this.setState({...this.state, class:"center noPrint upToTop hide Yalla"});
 	};
-	// applyRef = ref => {
-	// 	this.vn = ref;
-	// 	this.vn.addEventListener('scroll', this.myFunction);
-	// };
 	render() {
 		return (
-			<div className="center noPrint upToTop">
+			<div className={this.state.scrollTop > 200 ? "center noPrint upToTop" : "center noPrint upToTop"}>
 				<button onClick={this.handleClick.bind(this)} className="up"><FontAwesome name="chevron-up" /></button>
 			</div>
 		);
